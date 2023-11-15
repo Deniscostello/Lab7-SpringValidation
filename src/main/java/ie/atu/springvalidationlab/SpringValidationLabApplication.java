732dd88ff00f0a17e900c6cd0ac8c18b488645ca
Denis Costello <G00396309@atu.ie>
2023-11-15 09:10:26 +0000

    First commit

---
+++ src/main/java/ie/atu/springvalidationlab/SpringValidationLabApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.springvalidationlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringValidationLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringValidationLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/springvalidationlab/SpringValidationLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.springvalidationlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValidationLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
